46a55c6e46453b5b0badf1b85c946f268125da00
Pavol Hejný <me@pavolhejny.com>
2016-07-18 17:05:40 +0200

    refactoring

--- script/map.js
+++ script/map.js
@@ -28,46 +28,51 @@ $(function(){
 
 
         var blocks = '';
-        response.forEach(function (block) {
+        response.forEach(function (object) {
 
-            //console.log(block);
-
-            if(!block.material)return;
 
             var position = new BABYLON.Vector3(
-                block.position.x * -BLOCK_SIZE,
+                object.position.x * -BLOCK_SIZE,
                 BLOCK_SIZE*BLOCK_SIZE_VERTICAL/2,
-                block.position.y * BLOCK_SIZE
+                object.position.y * BLOCK_SIZE
             );
 
 
-            if(block.material=='wall' || block.material=='door') {
 
 
-                //Simple crate
-                var box = new BABYLON.Mesh.CreateBox("crate", BLOCK_SIZE, scene);
-                box.material = new BABYLON.StandardMaterial("Mat", scene);
-                box.material.diffuseColor = new BABYLON.Color3(0.2,0.2,0.2);
-                //box.material.diffuseTexture = new BABYLON.Texture("images/textures/crate.png", scene);
-                //box.material.diffuseTexture.hasAlpha = true;
-                box.position = position;
-                box.scaling.y = BLOCK_SIZE_VERTICAL;
-                box.checkCollisions = true;
+            if(object.type=='block') {
+                if (object.shape == 'wall' || object.shape == 'door') {
+
+                    //Simple crate
+                    var box = new BABYLON.Mesh.CreateBox("crate", BLOCK_SIZE, scene);
+                    box.material = new BABYLON.StandardMaterial("Mat", scene);
+                    box.material.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+                    //box.material.diffuseTexture = new BABYLON.Texture("images/textures/crate.png", scene);
+                    //box.material.diffuseTexture.hasAlpha = true;
+                    box.position = position;
+                    box.scaling.y = BLOCK_SIZE_VERTICAL;
+                    box.checkCollisions = true;
 
 
-                if(block.material=='door') {
-                    box.scaling.y = BLOCK_SIZE_VERTICAL-BLOCK_SIZE_DOOR;
-                    box.position.y += BLOCK_SIZE_DOOR*BLOCK_SIZE;
+                    if (object.material == 'door') {
+                        box.scaling.y = BLOCK_SIZE_VERTICAL - BLOCK_SIZE_DOOR;
+                        box.position.y += BLOCK_SIZE_DOOR * BLOCK_SIZE;
+                    }
+
+
                 }
-
-
             }else
-            if(block.material=='light'){
+            if(object.type=='light'){
 
-                r('creating light');
+                //r('creating light');
                 new BABYLON.PointLight("Omni", position, scene);
 
+
             }
+
+
+
+
 
 
 
